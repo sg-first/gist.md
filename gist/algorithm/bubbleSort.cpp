5510e794c68ea33477e4935d4b79fe513ccc5490
Mingkai Zhou <524019351@qq.com>
2018-11-08 08:56:05 +0800

    我瞎了

--- gist/algorithm/bubbleSort.cpp
+++ gist/algorithm/bubbleSort.cpp
@@ -3,7 +3,7 @@ void bubbleSort(vector<int>&arr)
     for(int i=0;i<arr.size()-1;i++) //计数，最坏情况遍历size-1次
     {
         bool changed=false;
-        for(int j=0;i<arr.size()-1-i;j++) //单次遍历，每次最右边都会多排好一个，所以那个不用看了
+        for(int j=0;j<arr.size()-1-i;j++) //单次遍历，每次最右边都会多排好一个，所以那个不用看了
         {
             if(arr[j]>arr[j+1])
             {
