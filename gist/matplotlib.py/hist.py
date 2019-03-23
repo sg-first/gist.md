c3423303c0c80a50bd8eac47b707638e382c0f9a
3A <524019351@qq.com>
2019-03-23 16:19:33 +0800

    hist

---
+++ gist/matplotlib.py/hist.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+# 参数依次为list,抬头,X轴标签,Y轴标签,XY轴的范围
+def draw_hist(myList,Title,Xlabel,Ylabel,Xrange,Yrange):
+    plt.hist(myList,100)
+    plt.xlabel(Xlabel)
+    plt.xlim(Xrange[0],Xrange[1])
+    plt.ylabel(Ylabel)
+    plt.ylim(Yrange[0],Yrange[1])
+    plt.title(Title)
+    plt.show()
