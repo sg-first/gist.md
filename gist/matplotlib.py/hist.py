e18588a40c1e3893a04d9f10162ffe6f08cedac0
Mingkai Zhou <524019351@qq.com>
2019-04-10 09:31:27 +0800

    默认参数

--- gist/matplotlib.py/hist.py
+++ gist/matplotlib.py/hist.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 
 # 参数依次为list,抬头,X轴标签,Y轴标签,XY轴的范围
-def draw_hist(myList,Title,Xlabel,Ylabel,Xrange,Yrange):
+def draw_hist(myList,Title="",Xlabel="",Ylabel="",Xrange,Yrange):
     plt.hist(myList,100)
     plt.xlabel(Xlabel)
     plt.xlim(Xrange[0],Xrange[1])
