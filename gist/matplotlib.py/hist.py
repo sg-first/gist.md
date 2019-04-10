25d877300cd283ee70d6923de9834d885f4ff766
Mingkai Zhou <524019351@qq.com>
2019-04-10 22:51:07 +0800

    Update hist.py

--- gist/matplotlib.py/hist.py
+++ gist/matplotlib.py/hist.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 
 # 参数依次为list,抬头,X轴标签,Y轴标签,XY轴的范围
 def draw_hist(myList,Title="",Xlabel="",Ylabel="",Xrange,Yrange):
-    plt.hist(myList,100)
+    plt.hist(myList,len(myList))
     plt.xlabel(Xlabel)
     plt.xlim(Xrange[0],Xrange[1])
     plt.ylabel(Ylabel)
