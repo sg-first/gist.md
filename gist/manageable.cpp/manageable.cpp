99b8d267c25af5a9e00de3a2b637fb2069aff520
3A <524019351@qq.com>
2021-01-05 20:53:53 +0800

    manageable.cpp

---
+++ gist/manageable.cpp/manageable.cpp
@@ -0,0 +1,25 @@
+#include "manageable.h"
+
+#ifdef autoRecovery
+
+manageContain* manageContain::contain;
+
+void managePair::del()
+{
+    if(*(this->isDelete)==false)
+    {
+        if(this->managePoint->isOwnership()) //没被删才能访问
+            delete managePoint; //用户持有所有权才删
+    }
+    delete isDelete; //无论如何都把这个bool删掉，因为manageContain析构函数遍历完所有managePair都没了
+}
+
+manageContain::~manageContain()
+{
+    for(managePair& pair : this->allObj)
+    {
+        pair.del();
+    }
+}
+
+#endif
