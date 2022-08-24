3b15ca9189fa0752f8566e9e9c82cc6998af223a
Mingkai Zhou <524019351@qq.com>
2022-08-24 20:59:32 +0800

    用大量预计算加速

--- gist/checkDuplicatePic.py/checkDuplicatePic.py
+++ gist/checkDuplicatePic.py/checkDuplicatePic.py
@@ -5,42 +5,41 @@ from numpy import average, dot, linalg
 # 对图片进行统一化处理
 def get_thum(image, size=(64, 64), greyscale=False):
     # 利用image对图像大小重新设置, Image.ANTIALIAS为高质量的
-    image = image.resize(size, Image.ANTIALIAS)
+    img2 = image.resize(size, Image.ANTIALIAS)
     if greyscale:
         # 将图片转换为L模式，其为灰度图，其每个像素用8个bit表示
-        image = image.convert('L')
-    return image
+        img2 = img2.convert('L')
+    return img2
 
-# 计算图片的余弦距离
-def image_similarity_vectors_via_numpy(image1, image2):
-    image1 = get_thum(image1)
-    image2 = get_thum(image2)
-    images = [image1, image2]
-    vectors = []
-    norms = []
-    for image in images:
-        vector = []
-        for pixel_tuple in image.getdata():
-            vector.append(average(pixel_tuple))
-        vectors.append(vector)
-        # 求图片的范数
-        norms.append(linalg.norm(vector, 2))
-    a, b = vectors
-    a_norm, b_norm = norms
-    res = dot(a / a_norm, b / b_norm)
-    return res
+def get_norm(image):
+    vector = []
+    for pixel_tuple in image.getdata():
+        vector.append(average(pixel_tuple))
+    return vector / linalg.norm(vector, 2)
+    
+class img:
+    def __init__(self, filename):
+        self.image = Image.open(filename)
+        self.normalizedNorm = get_norm(get_thum(self.image))
+
+def loadAll(allTexture, path):
+    ret={}
+    for i in allTexture:
+        ret[i] = img(path + i)
+    print('load finished')
+    return ret
 
 def detectFoladFolder(path):
     allTexture = FileUtils.GetFilenames(path, '.TGA')
     detected = set()
-    for i in allTexture:
+    textureMap = loadAll(allTexture, path)
+    
+    for i, image1 in textureMap.items():
         if not i in detected:
-            image1 = Image.open(path + i)
-            for j in allTexture:
+            for j, image2 in textureMap.items():
                 if (not j in detected) and i != j:
-                    image2 = Image.open(path + j)
-                    if image1.size == image2.size:
-                        cosin = image_similarity_vectors_via_numpy(image1, image2)
+                    if image1.image.size == image2.image.size:
+                        cosin = dot(image1.normalizedNorm, image2.normalizedNorm)
                         if cosin > 0.97:
                             print(i, j, cosin)
                             detected.add(i)
@@ -48,6 +47,5 @@ def detectFoladFolder(path):
     print(path, 'finished')
 
 if __name__ == "__main__":
-    detectFoladFolder('D:/texture/verysmall/')
     detectFoladFolder('D:/texture/small/')
     detectFoladFolder('D:/texture/big/')
